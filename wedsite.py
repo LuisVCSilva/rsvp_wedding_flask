b5138b0d44c229681e62fe4f485cf2dbf5f65f90
luis <luisviniciuscostasilva@gmail.com>
2025-01-07 01:58:04 -0300

    fix 500

--- wedsite.py
+++ wedsite.py
@@ -89,7 +89,7 @@ def page_not_found(e):
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('error_500.html'), 500
+    return render_template('500.html'), 500
     
 @app.errorhandler(403)
 def forbidden(e):
