37a3363be8cd845fd8187f8e715c4f41f00c5496
luis <luisviniciuscostasilva@gmail.com>
2025-01-06 21:43:47 -0300

    paginas de erro

--- wedsite.py
+++ wedsite.py
@@ -55,6 +55,23 @@ def pop_db():
         db.commit()  # Confirmar as inserções no banco de dados
 
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+@app.errorhandler(403)
+def forbidden(e):
+    return render_template('403.html'), 403
+
+@app.errorhandler(400)
+def bad_request(e):
+    return render_template('400.html'), 400
+
 # CREATE TABLE wishlist (id INTEGER PRIMARY KEY AUTOINCREMENT, label, desc, link, booked, changed);
 @app.route('/presentes.html', methods=['GET', 'POST'])
 def wishlist():
