d6cbdadc526bf3461ce314ed1d485fb0fafa5721
luis <luisviniciuscostasilva@gmail.com>
2025-01-07 01:35:40 -0300

    loop gif background home

--- wedsite.py
+++ wedsite.py
@@ -88,8 +88,8 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 @app.errorhandler(500)
-def internal_server_error(error):
-    return render_template('error_500.html', error=error), 500
+def internal_server_error(e):
+    return render_template('error_500.html'), 500
     
 @app.errorhandler(403)
 def forbidden(e):
